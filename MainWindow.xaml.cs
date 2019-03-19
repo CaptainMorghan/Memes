7796eab816aed05e596c14f230c619f2771f65a9
CaptainMorghan <47458916+CaptainMorghan@users.noreply.github.com>
2019-03-19 17:08:18 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Song
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCountryRoad_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("CountryRoads.txt");
+                lblOutput.Content = "";
+                while (!sr.EndOfStream)
+                {
+                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void BtnRickRoll_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("RickRoll.txt");
+                lblOutput.Content = "";
+                while (!sr.EndOfStream)
+                {
+                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnYMCA_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("YMCA.txt");
+                lblOutput.Content = "";
+                while (!sr.EndOfStream)
+                {
+                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnBitchLasangya_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("Bitch.txt");
+                lblOutput.Content = "";
+                while (!sr.EndOfStream)
+                {
+                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnOwo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("Owo.txt");
+                lblOutput.Content = "";
+                while (!sr.EndOfStream)
+                {
+                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
