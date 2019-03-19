4c6f3d6068dfa853bf87bd81c4f71ba1bd9441c1
CaptainMorghan <47458916+CaptainMorghan@users.noreply.github.com>
2019-03-19 18:13:30 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Song
+namespace junk
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,89 +24,37 @@ namespace Song
         {
             InitializeComponent();
         }
+        private void BtnClick_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int Song = rnd.Next(1, 2);
+            
 
-        private void BtnCountryRoad_Click(object sender, RoutedEventArgs e)
-        {
-            try
+            if (Song == 1)
             {
-                System.IO.StreamReader sr = new System.IO.StreamReader("CountryRoads.txt");
-                lblOutput.Content = "";
-                while (!sr.EndOfStream)
                 {
-                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                    System.IO.StreamReader sr = new System.IO.StreamReader("CountryRoads.txt");
+                    lblOutput.Content = "";
+                    while (!sr.EndOfStream)
+                    {
+                        lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-        private void BtnRickRoll_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                System.IO.StreamReader sr = new System.IO.StreamReader("RickRoll.txt");
-                lblOutput.Content = "";
-                while (!sr.EndOfStream)
-                {
-                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
 
-        private void BtnYMCA_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                System.IO.StreamReader sr = new System.IO.StreamReader("YMCA.txt");
-                lblOutput.Content = "";
-                while (!sr.EndOfStream)
+            }
+            else
+            
                 {
-                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                    System.IO.StreamReader sr = new System.IO.StreamReader("RickRoll.txt");
+                    lblOutput.Content = "";
+                    while (!sr.EndOfStream)
+                    {
+                        lblOutput.Content += sr.ReadLine() + Environment.NewLine;
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
+            
 
-        private void BtnBitchLasangya_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                System.IO.StreamReader sr = new System.IO.StreamReader("Bitch.txt");
-                lblOutput.Content = "";
-                while (!sr.EndOfStream)
-                {
-                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void BtnOwo_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                System.IO.StreamReader sr = new System.IO.StreamReader("Owo.txt");
-                lblOutput.Content = "";
-                while (!sr.EndOfStream)
-                {
-                    lblOutput.Content += sr.ReadLine() + Environment.NewLine;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
         }
     }
 }
+
